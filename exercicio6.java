6a7f0eddbbfeb61f9af6ed6b589cd6a4f6395be6
anaguimaraess <73199083+anaguimaraess@users.noreply.github.com>
2021-06-14 12:23:48 -0300

    Add files via upload

---
+++ exercicio6.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class exercicio6 {
+    public static void main(String[] args){
+
+        System.out.print("Digite o primeiro numero: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int n1 = scanner.nextInt();
+        System.out.print("Digite o segundo numero: ");
+
+        int n2 = scanner.nextInt();
+        System.out.print("Digite o terceiro numero: ");
+
+        int n3 = scanner.nextInt();
+        System.out.print("Digite o quarto numero: ");
+
+        int n4 = scanner.nextInt();
+        System.out.print("Digite o quinto numero: ");
+        
+        int n5 = scanner.nextInt();
+        System.out.print("Digite o sexto numero: ");
+
+        int n6 = scanner.nextInt();
+        System.out.print("Digite o setimo numero: ");
+
+        int n7 = scanner.nextInt();
+        System.out.print("Digite o oitavo numero: ");
+
+        int n8 = scanner.nextInt();
+        System.out.print("Digite o nono numero: ");
+
+        int n9 = scanner.nextInt();
+        System.out.print("Digite o decimo numero: ");
+
+        int n10 = scanner.nextInt();
+        System.out.print("A soma resultou em: " + (n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10));
+    }
+}
